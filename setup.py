eab9e29995c05a8a10069a0a1aefae0002915ac4
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2015-10-08 13:10:06 +0200

    Prepare next release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ name = 'seantis.plonetools'
 description = (
     "A collection of tools shared with different Seantis Plone modules."
 )
-version = '0.17'
+version = '0.18'
 
 # needed for tests and to use the default testcase of seantis.plonetools
 tests_require = [
